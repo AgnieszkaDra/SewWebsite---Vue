c4ed6cabede96544bbdd7d28a6b7ef010e92a786
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-03-22 18:47:18 +0100

    add toogle hamburger icon

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import HamburgerIcon from './components/icons/IconHamburger.vue'
+import CloseMenuIcon from './components/icons/IconCloseMenu.vue'
+import { ref } from 'vue';
+
+const navbarOpen = ref(false);
+
+const handleToggle = () => {
+  navbarOpen.value = !navbarOpen.value;
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header class="header">
+    <img alt="Vue logo" class="header__logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <div @click="handleToggle" class="menu-icon" :class="{ active: navbarOpen }">
+        <HamburgerIcon v-if="!navbarOpen" />
+        <CloseMenuIcon v-else />
+      </div>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -20,66 +33,72 @@ import HelloWorld from './components/HelloWorld.vue'
   <RouterView />
 </template>
 
-<style scoped>
-header {
+<style scoped lang="scss">
+.header {
   line-height: 1.5;
   max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  &__logo {
+    display: block;
+    margin: 0 auto 2rem;
+  }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
+  .wrapper {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    justify-content: space-between;
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
+  }
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+  nav a.router-link-exact-active {
+    color: var(--color-text);
+  }
+
+  nav a.router-link-exact-active:hover {
+    background-color: transparent;
+  }
+
+  nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+  }
+
+  nav a:first-of-type {
+    border: 0;
+  }
+
+  @media (min-width: 1024px) {
+    header {
+      padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .header .wrapper {
+      flex-wrap: wrap;
+    }
+
+    nav {
+      text-align: left;
+      margin-left: -1rem;
+      font-size: 1rem;
+      padding: 1rem 0;
+      margin-top: 1rem;
+    }
+  }
+
+  .menu-icon {
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  .menu-icon.active {
+    background-color: lightblue; /* Change this to the desired background color */
   }
 }
 </style>
