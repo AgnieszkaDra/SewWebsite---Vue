964684e459a26f24b2f1c0ea8c26006d9a7ebbb6
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-03-23 06:53:28 +0100

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import HamburgerIcon from './components/icons/IconHamburger.vue'
 import CloseMenuIcon from './components/icons/IconCloseMenu.vue'
+import Navigation from './components/Navigation/NavigationComponent.vue'
 import { ref } from 'vue';
 
 const navbarOpen = ref(false);
@@ -22,7 +23,7 @@ const handleToggle = () => {
         <HamburgerIcon v-if="!navbarOpen" />
         <CloseMenuIcon v-else />
       </div>
-
+      <Navigation :isNavbarOpen="navbarOpen"/> 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
