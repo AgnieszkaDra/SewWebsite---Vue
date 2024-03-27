b1dd1c411008941746e0f75c12a19d99b321a72e
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-03-27 06:17:26 +0100

    refactoring ChildView

---
+++ src/components/CategoriesContainer.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="section">
+    <div class="categories">
+      <!-- <CategoryComponent
+        v-for="product in products" 
+        :key="product.id" 
+        :name="product.name" 
+        :id="product.id" 
+        :background="product.background"
+        :collection="product.collection" 
+      /> -->
+
+      <div 
+      class="category__content"
+      v-for="product in products" 
+      :key="product.id"
+    >
+       <div class="category__span">Zobacz produkty</div> 
+    <h2 class="category__name">ffff </h2> <!-- Use product.name to display the name -->
+      <h3>Ola</h3>
+    </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useProductsStore } from '../stores/products.ts';
+
+const productsStore = useProductsStore();
+const products = computed(() => productsStore.products);
+</script>
+
+<script>
+// import CategoryComponent from '../components/CategoryComponent.vue';
+
+// export default {
+//   components: {
+//     CategoryComponent
+//   }
+// }
+</script>
+
+<style scoped>
+
+</style>
